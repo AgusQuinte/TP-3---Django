49144dd43e22de880132645020aa675f7993ce06
AgusQuinte <agusquintela08@gmail.com>
2025-07-10 15:43:58 -0300

    Entrega

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,18 @@ Including another URLconf
 """
 
 
-from django.contrib import admin
+from django.conf import settings
 from django.urls import include, path
+from django.contrib import admin
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    # Si tu app tiene URLs, las incluyes acá, ej:
+    # path('polls/', include('polls.urls')),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
